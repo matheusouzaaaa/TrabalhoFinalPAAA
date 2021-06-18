5c150d80046b9eb63cb810629574fab703975de8
Matheus Souza <matheusouzaaaa@gmail.com>
2021-06-17 21:31:36 -0300

    Adicionada um toolbar para contemplar o botão de logout do Firebase.auth

--- app/src/main/java/com/example/projetofinalpaaa/LoginActivity.kt
+++ app/src/main/java/com/example/projetofinalpaaa/LoginActivity.kt
@@ -4,9 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import com.example.projetofinalpaaa.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -19,6 +16,8 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -36,14 +35,6 @@ class LoginActivity : AppCompatActivity() {
             }
             }
 
-        }
-    }
-
-    public override fun onStart() {
-        super.onStart()
-        val currentUser = auth.currentUser
-        if(currentUser != null){
-            reload();
         }
     }
 
--- app/src/main/java/com/example/projetofinalpaaa/MainActivity.kt
+++ app/src/main/java/com/example/projetofinalpaaa/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.projetofinalpaaa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import com.example.projetofinalpaaa.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.logout -> {
+            logout()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun logout(){
+        Firebase.auth.signOut()
+        val intent = Intent (this, LoginActivity::class.java)
+        this.startActivity(intent)
     }
 }
